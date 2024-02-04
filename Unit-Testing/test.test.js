30316bd60818924ea72b4f39e559564eb5b01ad7
peaceGloriole <dmtrpwlw@icloud.com>
2024-02-04 19:22:53 +0200

    request and evenOdd are done with tests for second

--- Unit-Testing/test.test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe(`my first test`, () => {
-  it(`test`, () => {
-    expect(false, `Custom message: it should be truthy`).to.be.ok;
-  });
-});
---
+++ Unit-Testing/tests/evenOrOdd.test.js
@@ -0,0 +1,24 @@
+import { isOddOrEven } from "../evenOrOdd/evenOrOdd.js";
+import { expect } from 'chai';
+
+describe(`Unit Test for isOddOrEven()`, () => {
+  it(`should return "even" for a string with even length`, () => {
+    const result = isOddOrEven('even');
+    expect(result).to.equal('even');
+  });
+
+  it(`should return "odd" for a string with odd length`, () => {
+    const result = isOddOrEven('odd');
+    expect(result).to.equal('odd');
+  });
+
+  it(`should return undefined for a non-string input`, () => {
+    const result = isOddOrEven(123);
+    expect(result).to.be.undefined;
+  });
+
+  it(`should return undefined for an undefined input`, () => {
+    const result = isOddOrEven();
+    expect(result).to.be.undefined;
+  });
+});
