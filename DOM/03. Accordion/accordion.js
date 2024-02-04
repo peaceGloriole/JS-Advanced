eeb4327e50159073e2c253d80bc53c3f3a434eb5
peaceGloriole <dmtrpwlw@icloud.com>
2024-02-04 22:03:52 +0200

    nothing major

--- DOM/03. Accordion/accordion.js
+++ DOM/03. Accordion/accordion.js
@@ -1,21 +1,22 @@
 function toggle() {
-    let btnRef = document.querySelector(`.button`);
-    let hiddenInfo = document.getElementById(`extra`);
+    let btnRef = document.querySelector('.button');
+    let hiddenInfo = document.getElementById('extra');
 
-    btnRef.addEventListener(`click`, clickHandler);
-    
+    btnRef.addEventListener('click', clickHandler);
+
     function clickHandler(event) {
         let btnText = (event.currentTarget.textContent === 'Less') ? 'More' : 'Less';
         event.currentTarget.textContent = btnText;
 
         hiddenInfo.style.display = (hiddenInfo.style.display === 'block') ? 'none' : 'block';
 
-    //     // if (btnRef.textContent === 'Less') {
-    //     //     btnRef.textContent = 'More';
-    //     //     hiddenInfo.style.display = 'none';
-    //     // } else {
-    //     //     btnRef.textContent = 'Less';   
-    //     //     hiddenInfo.style.display = 'block';
-    //     // }
+        // //     // if (btnRef.textContent === 'Less') {
+        // //     //     btnRef.textContent = 'More';
+        // //     //     hiddenInfo.style.display = 'none';
+        // //     // } else {
+        // //     //     btnRef.textContent = 'Less';   
+        // //     //     hiddenInfo.style.display = 'block';
+        // //     // }
+        // }
     }
 }
